b6537ba0a814012948bc6303a069dd598ec91cdf
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2022-10-11 09:52:18 -0500

    Normalize scan logic for user input

--- scan.py
+++ scan.py
@@ -1,19 +1,11 @@
 def logscan(text):
     with open(text, 'r') as searchfile:
         for line in searchfile:
-            if 'LiquiGlobal updates failed on MASTER from the change log at' in line:
-                print (line + '\n   See: https://kcura.lightning.force.com/lightning/r/ka01T000000QQxUQAW/view for more information.\n')
-            elif 'The ResultSet is closed' in line:
-                print (line + '\n   See: https://kcura.lightning.force.com/lightning/r/ka01T000000UkQ4QAK/view for more information.\n')
-            elif 'waiting for server to start........ stopped waiting' in line:
-                print (line + '\n   See: https://kcura.lightning.force.com/lightning/r/ka01T000000UmCiQAK/view for more information.\n')
-            elif 'LOW_MEMORY' in line:
-                print (line + '\n   See: https://kcura.lightning.force.com/lightning/r/ka01T000000UjDOQA0/view for more information.\n')
-            elif 'unknown protocol' in line:
-                print (line + '\n   See: https://kcura.lightning.force.com/lightning/r/ka01T000000UpMMQA0/view for more information.\n')
-            elif 'Keystore was tampered with, or password was incorrect' in line:
-                print (line + '\n   See: https://kcura.lightning.force.com/lightning/r/ka01T000000UieiQAC/view for more information.\n')
-            elif 'Address already in use: bind' in line:
-                print (line + '\n   See: https://kcura.lightning.force.com/lightning/r/ka01T000000UihzQAC/view for more information.\n')
+            if '<<Insert Exception 1 Message>>' in line:
+                print (line + '\n   See: This SOP Link. \n')
+            elif '<<Insert Exception 2 Message>>' in line:
+                print (line + '\n   See: This SOP Link.\n')
+            #elif '<<Additional Exceptions Message>>' in line:
+                #print (line + '\n   See: This SOP Link.\n')
         else:
             print ('We couldn\'t find any other errors')
