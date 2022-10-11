142cf5ed89d631cc5b87b93e4721f06ab165d81f
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2022-10-11 09:56:41 -0500

    Update logscan.py

--- logscan.py
+++ logscan.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import os.path
 from scan import logscan
 
 sg.theme('Reddit')	# Add theme
@@ -10,17 +11,20 @@ layout = [  [sg.Text('Select Log File:')],
             [sg.Output(size=(100, 20))] ]
 
 # Create the Window
-window = sg.Window('LogScan v1', layout, icon='logscan.ico')
+window = sg.Window('LogScan', layout, icon='logscan.ico')
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
+    input_text = str(values['_file_'])
+    is_file = os.path.isfile(input_text)
     if event == sg.WIN_CLOSED or event == 'Cancel':	# if user closes window or clicks cancel
         break
     if event == 'Scan':
-        if values['_file_']:
+        if input_text == 'about':
+            print('\nThis is Logscan Version <<VersionNumber>>.\n \nVisit: https://SOP.com/information>> for more information.\n\n')
+        elif is_file is False:
+            print('\nThis is not a valid file. Please check the file path.')
+        elif values['_file_']:
             logscan(values['_file_'])
-        elif values['']:
-            print('we could not find this file')
-
 window.close()
